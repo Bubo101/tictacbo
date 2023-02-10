0e52836a4ce8666955cf74205cb1c8c0166dd7cb
Boden <boden.bradley@gmail.com>
2023-02-10 14:43:42 -0800

    Improved CSS stylin for game

--- src/components/Game.js
+++ src/components/Game.js
@@ -40,19 +40,21 @@ const Game = () => {
         })
     return (
         <>
-        <h1>Tic Tac Toe</h1>
-        <h2>Created with React Hooks & Deployed with AWS</h2>
-        <h4>by Boden Bradley - Software Developer</h4>
+        <div className="page">
+            <h1>Tic Tac Toe</h1>
+            <h2>Created with React Hooks & Deployed with AWS</h2>
+            <h4>by Boden Bradley - Software Developer</h4>
 
-        <Board squares={history[stepNumber]} onClick={handleClick} />
-        <div className="info-wrapper">
-            <div>
-            <h3>History</h3>
-            {renderMoves()}
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div className="info-wrapper">
+                <div>
+                <h3>History</h3>
+                {renderMoves()}
+                </div>
+                <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
             </div>
-            <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+            <h5>With help from codeSTACKr youtube tutorial</h5>
         </div>
-        <h5>With help from codeSTACKr youtube tutorial</h5>
         </>
     )
 }
