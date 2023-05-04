44aeb538304351677421f7bb2b6f952277c7f8ab
Boden <boden.bradley@gmail.com>
2023-05-03 19:47:01 -0700

    updated game

--- src/components/Game.js
+++ src/components/Game.js
@@ -42,7 +42,7 @@ const Game = () => {
         <>
         <div className="page">
             <h1>Tic Tac Toe/Bo</h1>
-            <h2>Created w/ React Hooks & Deployed with AWS Amplify Online</h2>
+            <h2>Created w/ React Hooks & Deployed with AWS Amplify</h2>
             <h4>by Boden Bradley - Full Stack Software Developer</h4>
 
             <Board squares={history[stepNumber]} onClick={handleClick} />
